7c58301ed43453bec5890a0631faefaa4802a481
Warren Santiago <wsantiago9623@gmail.com>
2022-09-12 16:08:17 -0400

    commit tree 20eb9fa1a67a8f75fdcdf714ecf883c9a66551c8

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "root is the champion"
+
+
+
+
